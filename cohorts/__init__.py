0398051e0780ff7fecac07f5cc38de791f152238
Joshua Wise <joshua@joshuawise.com>
2026-07-03 22:49:34 -0400

    beeline: add environment variable for W3C trace header support

--- cohorts/__init__.py
+++ cohorts/__init__.py
@@ -1,5 +1,6 @@
 import beeline
 from beeline.middleware.flask import HoneyMiddleware
+import beeline.propagation.w3c as w3c
 from flask import Flask
 
 from .api import init_app as init_api
@@ -11,7 +12,12 @@ app = Flask(__name__)
 app.config.update(config)
 
 if app.config["HONEYCOMB_KEY"]:
-    beeline.init(writekey=app.config["HONEYCOMB_KEY"], dataset="rws", service_name="cohorts")
+    if os.environ.get('O11Y_SHOULD_USE_W3C_TRACE_HEADERS', False):
+        # Only turn this on once EVERYTHING has migrated to the new
+        # rws_common version that supports W3C trace headers.
+        beeline.init(writekey=app.config["HONEYCOMB_KEY"], dataset="rws", service_name="cohorts", http_trace_propagation_hook=w3c.http_trace_propagation_hook)
+    else:
+        beeline.init(writekey=app.config["HONEYCOMB_KEY"], dataset="rws", service_name="cohorts")
     HoneyMiddleware(app)
 
 init_models(app)
